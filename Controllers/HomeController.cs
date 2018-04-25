f376f269cec9675324281f0ffb5c0c6d0a1ee280
Luke Ngugi <luke.ngugi@gmail.com>
2018-04-25 17:34:30 +0300

    Added Connect to DB

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.MainPage = "User Management";
+            ViewBag.SubPage = "Home";
+            return View();
+        }
+        public ActionResult Users()
+        {
+            ViewBag.MainPage = "User Management";
+            ViewBag.SubPage = "Manage Users";
+
+            return RedirectToRoute("Users");
+        }
+        public ActionResult Roles()
+        {
+            ViewBag.MainPage = "User Management";
+            ViewBag.SubPage = "User Roles";
+
+            return RedirectToRoute("Roles");
+        }
+    }
+}
---
+++ Models/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserMVC.Model
+{
+    public class Roles
+    {
+        public int RoleID { get; set; }
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+        public int RoleStatus { get; set; }
+    }
+}
